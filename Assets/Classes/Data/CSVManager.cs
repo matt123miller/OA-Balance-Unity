4f40877e407c8719f2ab4b126580c315bc02c2b3
matt123miller Laptop <matt123miller@hotmail.co.uk>
2017-03-25 19:22:53 +0000

    Trying to read in CSV files, failing so far.

--- Assets/Classes/Data/CSVManager.cs
+++ Assets/Classes/Data/CSVManager.cs
@@ -4,21 +4,51 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using Random = UnityEngine.Random;
 
 public class CSVManager : MonoBehaviour
 {
+    public enum CSVState
+    {
+        read = 0,
+        write = 1
+    }
 
+    [SerializeField]
+    public CSVState state = 0;
     public string filePath;
     public string csvTitle;
-    public bool escapeQuotes = false;
-    public bool fullySplitVector = false;
     public Text textField;
 
-    void Start()
+    void Awake()
     {
         filePath = SetupFilePath();
         Debug.Log(filePath);
+        if (state == CSVState.read)
+        {
+            // Load the file in and send it to the right place.
+            StreamReader sr = new StreamReader(filePath);
+            var lines = new List<string[]>();
+            int Row = 0;
+            print(sr.ReadToEnd());
+            // Y U NO READ?!?!
+            //while (!sr.EndOfStream)
+            //{
+            //    string[] strInfo = sr.ReadLine().Split(',');
+            //    Vector2 v2 = new Vector2();
+            //    print(strInfo);
+            //    lines.Add(strInfo);
+            //    Row++;
+            //    Console.WriteLine(Row);
+            //}
+
+            //var data = lines.ToArray();
+            //print(data);
+            // Send it to the right place
+        }
         textField.text = filePath;
     }
 
@@ -38,15 +68,7 @@ public class CSVManager : MonoBehaviour
             {
                 textOutput.Append(",");
                 // Hopefully no array overrun
-                //if (fullySplitVector)
                 vector = (double)list[i].x + "," + (double)list[i].y;
-
-                //else if (!fullySplitVector && escapeQuotes)
-                //vector = "\"" + list[i].ToString() + "\"";
-
-                //else
-                //    vector = list[i].ToString();
-
 
                 textOutput.Append(vector);
             }
@@ -57,28 +79,60 @@ public class CSVManager : MonoBehaviour
         System.IO.File.AppendAllText(filePath, textOutput.ToString());
     }
 
+
+
     private string SetupFilePath()
     {
-        // All this just decides what to name the file
+        DirectoryInfo dir = new DirectoryInfo(GetPath() + "/CSV/"); 
 
-        DirectoryInfo dir;
+        switch (state)
+        {
+            case CSVState.read:
+                dir = new DirectoryInfo(GetPath() + "/CSV/Read/");
+                var readFiles = ShuffleArray(dir.GetFiles());
+                // choose a random file 
+                string path = "";
+                for (int i = 0; i < readFiles.Length; i++)
+                {
+                    var file = readFiles[i];
+                    if (!file.Extension.Contains("meta"))
+                    {
+                        path = file.Directory + "\\" + file.Name;
+                    }
+
+                }
+                print(path);
+                // I have a path so now read the fi
+
+                break;
+            case CSVState.write:
+                // All this just decides what to name the file
+
 #if UNITY_EDITOR
-        dir = new DirectoryInfo(GetPath() + "/CSV/");
+
 #else
         // This is the final build option
         dir = new DirectoryInfo(GetPath());
 #endif
-        FileInfo[] files = dir.GetFiles();
-        int csvTotal = 1;
 
-        foreach (FileInfo file in files)
-            if (file.Extension == ".csv")
-                csvTotal += 1;
+                FileInfo[] writeFiles = dir.GetFiles();
+                int csvTotal = 1;
 
-        if (csvTitle == "")
-            csvTitle = "Participant" + csvTotal + ".csv";
-        else // save the file as whatever the user inputs.
-            csvTitle += ".csv";
+                foreach (FileInfo file in writeFiles)
+                    if (file.Extension == ".csv")
+                        csvTotal += 1;
+
+                if (csvTitle == "")
+                    csvTitle = "Participant" + csvTotal + ".csv";
+                else // save the file as whatever the user inputs.
+                    csvTitle += ".csv";
+
+                // Then this will actually create the path using the gile name
+                //return dir.ToString() + csvTitle;
+                break;
+            default:
+                break;
+        }
 
         // Then this will actually create the path using the gile name
         return dir.ToString() + csvTitle;
@@ -97,5 +151,18 @@ public class CSVManager : MonoBehaviour
 #endif
         return Application.dataPath;
         // Will I have to manually add windows/mac support here?
+    }
+
+    T[] ShuffleArray<T>(T[] array)
+    {
+        var r = new System.Random();
+        for (int i = array.Length; i > 0; i--)
+        {
+            int j = r.Next(i);
+            T k = array[j];
+            array[j] = array[i - 1];
+            array[i - 1] = k;
+        }
+        return array;
     }
 }
